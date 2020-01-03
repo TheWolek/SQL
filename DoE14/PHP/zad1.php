24619463d8e4fce06eef7f26f69359b0160cf9a0
TheWolek <pawelm916@wp.pl>
2020-01-03 15:47:49 +0100

    tak

--- DoE14/PHP/zad1.php
+++ DoE14/PHP/zad1.php
@@ -7,6 +7,17 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        $conn = mysqli_connect('localhost','root','132','ogloszenia');
+        $sql = 'select id, tytul, tresc from ogloszenie where kategoria like 1';
+        $result = mysqli_query($conn,$sql);
+
+        if(mysqli_num_rows($result) > 0) {
+            while($row=mysqli_fetch_array($result)) {
+                echo $row[0];
+                echo "<br/>";
+            }
+        }
+    ?>
 </body>
 </html>
